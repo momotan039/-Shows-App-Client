bd4d1db9d44c305938d8dd54cfc40d89dc180e68
MOHAMMED <momotaha039@gmail.com>
2023-04-08 00:14:02 +0300

    add main and header and footer

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../components/header/Header'
+import Footer from '../components/footer/Footer'
 
 function Root() {
   return (
     <>
-    <div>Root</div>
+    <Header/>
+    <main>
     <Outlet/>
+    </main>
+    <Footer/>
     </>    
   )
 }
