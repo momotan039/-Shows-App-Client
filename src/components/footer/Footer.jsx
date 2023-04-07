13849b9d4e4892346e975b60030d0da4667f7185
MOHAMMED <momotaha039@gmail.com>
2023-04-08 00:12:31 +0300

    add footer component and style

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; // Import CSS file for styling
+
+const Footer = () => {
+  return (
+    <footer className="footerContainer">
+      <p className="footerText">© 2023 Shows App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
