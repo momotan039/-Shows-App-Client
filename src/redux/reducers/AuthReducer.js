77355ee1941fe241fb8c2f2142d4fe8e9a784d84
MOHAMMED <momotaha039@gmail.com>
2023-04-08 00:15:56 +0300

    add auth reducer

--- src/redux/reducers/AuthReducer.js
+++ src/redux/reducers/AuthReducer.js
@@ -1,3 +1,30 @@
-const authReducer=(state,action)=>{
-    
+const initialState = {
+    user: null,
+    error:null
+  };
+
+const authReducer=(state=initialState,action)=>{
+    switch(action.type)
+    {
+        case 'SIGN_IN':
+            return{
+                ...state,
+                user:action.payload,
+            }
+        case 'SIGN_UP':
+            return{
+                ...state,
+                user:action.payload
+            }
+            case 'ERORR':
+                return{
+                    ...state,
+                    error:action.payload
+                }
+        default:
+            return state
+    }
 }
+
+
+export default authReducer
