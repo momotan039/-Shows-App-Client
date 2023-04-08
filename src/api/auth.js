6d3017769bec15621e9be5373cc4126e8f6e7ab7
momotan <momotaha039@gmail.com>
2023-04-09 00:31:26 +0300

    edit auth functions

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,32 +1,14 @@
 import axios from "axios";
 import { API } from "../utils/constance";
-// export const signIn=async(user)=>{
-//     const res=await axios.post(API+'/login',user)
-//     return res.data
-// }
 
 export const signIn=async(user)=>{
-   new Promise((res,rej)=>{
-        setTimeout(() => {
-            res({
-                    "_id": "642f14e62f7de1b54b083c05",
-                    "email": "momotaha039@gmail.com",
-                    "name": "mohammed taha",
-                    "preferences": {
-                        "lang": "en",
-                        "show_lang": "fr",
-                        "genres": [
-                            {
-                                "id": 35,
-                                "name": "Comedy"
-                            },
-                            {
-                                "id": 80,
-                                "name": "Crime"
-                            }
-                        ]
-                    }
-            })
-        }, 2000);
-   })
+    const res=await axios.post(API+'/login',user,{
+        withCredentials:true
+    })
+    return res.data
+}
+
+export const signUp=async(data)=>{
+    const res=await axios.post(API+'/register',data)
+    return res.data
 }
