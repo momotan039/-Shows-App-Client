24544306c142b7ee8c448b417db5624feeb23dd2
momotan <momotaha039@gmail.com>
2023-04-09 17:46:05 +0300

    add temp menu and logout icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,30 @@
 import {useEffect} from 'react';
 import './Header.css'; // Import CSS file for styling
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { logout } from '../../redux/actions/AuthActions';
 
 const Header = () => {
+  const dispatch=useDispatch()
   const {user}=useSelector(state=>state.auth)
   return (
-    <header className="headerContainer">
+    <header className={`${user?'space':''}`}>
     <div className="logoContainer">
-        <h1 className="logoText">Shows App</h1>
-        {/* user:
-        {
-        user&&<h1>{user.name}</h1>
-        } */}
+        <Link to='/'><h1 className="logoText">Shows App</h1></Link>
       </div>
+        <div className="controls">
+        <nav className='menu'>
+          <ul>
+            <NavLink to='/dashborad'>DashBoard</NavLink>
+            <NavLink to='/setup-profile'>Setup-profile</NavLink>
+          </ul>
+        </nav>
+        <div className="log-out">
+          {user&&<h3>hello {user.name.split(' ')[0]}</h3>}
+          <AiOutlineLogout onClick={()=>logout(dispatch)} size={30}/>
+        </div>
+        </div>
     </header>
   );
 };
