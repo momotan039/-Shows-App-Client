d4dc805e74f12188a77c97c60d7518daa7222450
momotan <momotaha039@gmail.com>
2023-04-09 17:46:32 +0300

    add new routs

--- src/components/MyRouterProvider.jsx
+++ src/components/MyRouterProvider.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Root from "../pages/Root";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SignIn from "../pages/sgin in/SignIn";
 import SignUp from "../pages/sgin up/SignUp";
+import Dashboard from "../pages/Dashboard";
+import SetupProfile from "./setup profile/SetupProfile";
+import { getUserfromStorage } from "../utils/localStorage";
 
 export const MyRouterProvider = () => {
+
   const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getUserfromStorage(dispatch);  
+  }, []);
+
   const generateRouter = () => {
     const withUser = createBrowserRouter([
       {
@@ -17,6 +26,14 @@ export const MyRouterProvider = () => {
           {
             path: "/",
             element: <Home />,
+          },
+          {
+            path: "/dashborad",
+            element: <Dashboard />,
+          },
+          {
+            path: "/setup-profile",
+            element:<SetupProfile/>
           },
         ],
       },
@@ -37,7 +54,6 @@ export const MyRouterProvider = () => {
         ],
       },
     ]);
-
     return user ? withUser : withOutUser;
   };
   return <RouterProvider router={generateRouter()} />;
