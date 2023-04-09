14dd4d92069c27a4bc347ea3c0a252dc792854b1
momotan <momotaha039@gmail.com>
2023-04-09 17:41:54 +0300

    edit imports app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import "./App.css";
-import Root from "./pages/Root";
-import Home from "./pages/Home";
 import { MyRouterProvider } from "./components/MyRouterProvider";
 import { Provider } from "react-redux";
 import store from "./redux/store";
