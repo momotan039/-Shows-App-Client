21fefb9ff33ab931bffd373a7be71d500f9b8ea7
momotan <momotaha039@gmail.com>
2023-04-09 17:44:31 +0300

    add fetch users function

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,10 +5,17 @@ export const signIn=async(user)=>{
     const res=await axios.post(API+'/login',user,{
         withCredentials:true
     })
-    return res.data
+   return res.data
 }
 
 export const signUp=async(data)=>{
     const res=await axios.post(API+'/register',data)
     return res.data
+}
+
+export const fetchUsers=async()=>{
+    const data=await fetch(API+'/api/v1/users',{
+        credentials:'include'
+    }).then(res=>res.json());
+    return data
 }
