1be6a358550967fd3b1c0d0ce6c0581f50faa049
momotan <momotaha039@gmail.com>
2023-04-09 17:44:10 +0300

    reformat

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from '../components/header/Header'
-import Footer from '../components/footer/Footer'
-import Loader from '../components/loader/Loader'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Loader from "../components/loader/Loader";
 
 function Root() {
   return (
     <>
-    <Header/>
-    <main>
-    <Outlet/>
-    </main>
-    <Footer/>
-    <Loader/>
-    </>    
-  )
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+      <Loader />
+    </>
+  );
 }
 
-export default Root
+export default Root;
