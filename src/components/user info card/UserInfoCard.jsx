32abe17f6512f6a5d48559084993dcb5326f41dd
momotan <momotaha039@gmail.com>
2023-04-11 12:58:50 +0300

    add user info card

---
+++ src/components/user info card/UserInfoCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import "./UserInfoCard.css"; 
+
+function UserInfoCard() {
+    const { user } = useSelector((state) => state.account);
+    const { email, name } = user;
+    const { lang, show_lang, genres } = user.preferences;
+  return (
+    <div className="user-info-container">
+    <h2 className="user-info-title">Welcome to Your user-info</h2>
+    <div className="user-info-item">
+      <span className="user-info-label">Email:</span> {email}
+    </div>
+    <div className="user-info-item">
+      <span className="user-info-label">Name:</span> {name}
+    </div>
+    <div className="user-info-item">
+      <span className="user-info-label">Language:</span> {lang}
+    </div>
+    <div className="user-info-item">
+      <span className="user-info-label">Show Language:</span> {show_lang}
+    </div>
+    <div className="user-info-item">
+      <span className="user-info-label">Genres:</span>
+      <ul>
+        {genres.map((genre, index) => (
+          <li key={index}>
+            ID: {genre.id}, Name: {genre.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className="user-info-footer">Enjoy Your Experience!</div>
+  </div>
+  )
+}
+
+export default UserInfoCard
