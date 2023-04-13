ee18a9a7cf3a1be946f89b2c9a61f8e87d19cc3e
MOHAMMED <momotaha039@gmail.com>
2023-04-13 03:26:05 +0300

    edit home page adding these components -search bar -trending shows -recommended shows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react'
 import { fetchUsers } from '../api/auth'
+import SearchBar from '../components/search bar/SearchBar'
+import TrendingShows from '../components/trending shows/TrendingShows'
+import RecommendedShows from '../components/recommended shows/RecommendedShows'
 
 function Home() {
- 
   return (
-    <div>Home</div>
+    <div className="home">
+      <SearchBar/>
+      <TrendingShows/>
+      <RecommendedShows/>
+    </div>
   )
 }
 
