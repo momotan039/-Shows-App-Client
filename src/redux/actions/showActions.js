deb5ea1b79e79824ee9bb1c8e4661c7d96cdd123
momotan <momotaha039@gmail.com>
2023-04-15 15:52:34 +0300

    add actions for shows users

--- src/redux/actions/showActions.js
+++ src/redux/actions/showActions.js
@@ -6,6 +6,28 @@ export const selectShow=(show)=>{
    } 
 }
 
+export const addToFavorite=(show)=>{
+   return{
+        type:'ADD_TO_FAVORITE',
+        payload:show
+   } 
+}
+
+export const addToWatchLater=(show)=>{
+   return{
+        type:'ADD_TO_WATCH_LATER',
+        payload:show
+   } 
+}
+
+export const addToViewed=(show)=>{
+   return{
+        type:'ADD_TO_VIEWED',
+        payload:show
+   } 
+}
+
+
 export const setGenresShows=(genres)=>{
    return{
         type:'SET_GENRES_SHOWS',
