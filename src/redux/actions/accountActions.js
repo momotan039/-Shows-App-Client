f7af235d9daaa63fb5a7d677289715de827099b1
momotan <momotaha039@gmail.com>
2023-04-15 15:53:04 +0300

    add users shows actions

--- src/redux/actions/accountActions.js
+++ src/redux/actions/accountActions.js
@@ -22,3 +22,51 @@ export const setCurrentUser=(user)=>{
         payload: user,
       };
 }
+
+export const addFavoriteShow=(show,user)=>{
+  // const favorites=Array.from(user.favorite)
+  // favorites.push(show)
+  const temp=Object.assign({},user)
+  temp.favorite.push(show)
+  return {
+    type: "ADD_FAVORITE_SHOW",
+    payload: temp,
+  };
+}
+
+export const removeFavoriteShow=(show,user)=>{
+  const favorites=user.favorite.filter(f=>f.id!==show.id)
+  return {
+    type: "REMOVE_FAVORITE_SHOW",
+    payload: favorites,
+  };
+}
+
+export const addViewedShow=(show)=>{
+  return {
+    type: "ADD_VIEWED_SHOW",
+    payload: show,
+  };
+}
+
+export const removeViewedShow=(show)=>{
+  return {
+    type: "REMOVE_VIEWED_SHOW",
+    payload: show,
+  };
+}
+
+
+export const addShowLaterShow=(show)=>{
+  return {
+    type: "ADD_SHOW_LATER_SHOW",
+    payload: show,
+  };
+}
+
+export const removeShowLaterShow=(show)=>{
+  return {
+    type: "REMOVE_SHOW_LATER_SHOW",
+    payload: show,
+  };
+}
