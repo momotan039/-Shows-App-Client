cc5dc50aa232197110aab63a7d41a04e40ebb979
momotan <momotaha039@gmail.com>
2023-04-15 15:51:20 +0300

    add user shows actions

--- src/redux/reducers/showReducer.js
+++ src/redux/reducers/showReducer.js
@@ -9,6 +9,12 @@ export default (state = initialState, { type, payload }) => {
     return { ...state, show:payload }
   case 'SET_GENRES_SHOWS':
     return {...state,genres:payload}
+  case 'ADD_TO_FAVORITE':
+    return {...state,show:payload}
+  case 'ADD_TO_WATCH_LATER':
+    return {...state,show:payload}
+  case 'ADD_TO_VIEWED':
+    return {...state,show:payload}
   default:
     return state
   }
