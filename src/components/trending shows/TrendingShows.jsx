05bcd5f5a16b5c220506fdcbb1c83b1f98623c13
momotan <momotaha039@gmail.com>
2023-04-16 20:46:04 +0300

    add delying time

--- src/components/trending shows/TrendingShows.jsx
+++ src/components/trending shows/TrendingShows.jsx
@@ -1,28 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import Shows from '../shows/Shows'
-import { getTrendingShows } from '../../api/shows'
-import { useDispatch } from 'react-redux'
-import { showLoader } from '../../redux/actions/loaderActions'
+import React, { useEffect, useState } from "react";
+import Shows from "../shows/Shows";
+import { getTrendingShows } from "../../api/shows";
+import { useDispatch } from "react-redux";
+import { showLoader } from "../../redux/actions/loaderActions";
 
 function TrendingShows() {
-    const [shows,setShows]=useState(null)
-    const dispatch=useDispatch()
-    const _getTrendingShows=async()=>{
-        try {
-            const data=await getTrendingShows()
-            setShows(data.results)
-        } catch (error) {
-            alert(error.response.data.message)
-        }
+  const [shows, setShows] = useState(null);
+  const dispatch = useDispatch();
+  const _getTrendingShows = async () => {
+    try {
+      const data = await getTrendingShows();
+      setTimeout(() => {
+        setShows(data.results);
+      }, 2000);
+    } catch (error) {
+      alert(error.response.data.message);
     }
-    useEffect(()=>{
-        _getTrendingShows()
-    },[])
+  };
+  useEffect(() => {
+    _getTrendingShows();
+  }, []);
   return (
-    <div className='trending'>
-    <Shows title='Trending Shows' shows={shows}/>
+    <div className="container">
+      <div className="trending">
+        <Shows title="Trending Shows" shows={shows} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default TrendingShows
+export default TrendingShows;
