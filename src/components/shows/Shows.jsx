b145397b3a3b0991230ca5f5d3af489990b501ab
momotan <momotaha039@gmail.com>
2023-04-16 20:44:31 +0300

    refresh user state after remove show

--- src/components/shows/Shows.jsx
+++ src/components/shows/Shows.jsx
@@ -1,33 +1,55 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { removeShowFromUserShows } from "../../api/shows";
+import { removeFavoriteShow, removeViewedShow, removeWatchLaterShow } from "../../redux/actions/accountActions";
 import { hideLoader, showLoader } from "../../redux/actions/loaderActions";
 import ShowCard from "../show card/ShowCard";
+import SkeltonShows from "../skelton shows/SkeltonShows";
+import SkeltonShow from "../skelton shows/SkeltonShows";
 import "./Shows.css"; 
 
-const Shows = ({title,shows,apiRoute,enableDelete}) => {
+const Shows = ({title,shows,apiRoute,enableDelete,setShows}) => {
   const dispatch=useDispatch()
   const deleteShow=async (id)=>{
-    
-      dispatch(showLoader())
+    return new Promise(async (res,rej)=>{
       try {
-        const data= await removeShowFromUserShows(id,apiRoute)
-        console.log(data)
-      } catch (error) {
+      dispatch(showLoader())
+      const data= await removeShowFromUserShows(id,apiRoute)
+      alert(data.message)
+      setTimeout( () => {
+          setShows(shs=>shs.filter(f=>f.id!==id))
+          switch (apiRoute) {
+            case 'favorite':
+              dispatch(removeFavoriteShow(id))
+              break;
+            case 'watch-later':
+              dispatch(removeWatchLaterShow(id))
+              break;
+            case 'viewd':
+              dispatch(removeViewedShow(id))
+              break;
+          
+            default:
+              break;
+          }
+          dispatch(hideLoader())
+          res()
+        }, 1000);
+      } 
+      catch (error) {
         console.log(error.response.data.message)
+        rej()
       }
-      dispatch(hideLoader())
+    })
+      
   }
 
-  if(!shows)
-  return <ShowCard isSkelton={true}/>
-
-   if(shows.length>0)
   return (
     <div className="shows-container">
       <h2 className="shows-title">{title}</h2>
       <div className="shows-grid">
-        {shows.map((show) => (
+        {!shows&&<SkeltonShows/>}
+        {shows &&shows.length>0&&shows.map((show) => (
           <ShowCard
            deleteMe={enableDelete?()=>deleteShow(show.id):null}
             key={show._id||show.id}
