18a87d9f2a02a0808eb6d13c6b183bb16a23fcb6
momotan <momotaha039@gmail.com>
2023-04-17 20:18:48 +0300

    hide scroller and show it after serach

--- src/components/search bar/SearchBar.jsx
+++ src/components/search bar/SearchBar.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import axios from "axios";
-import Select from "react-select";
 import { FiSearch } from "react-icons/fi";
 import "./SearchBar.css";
 import { getSearchedShows } from "../../api/shows";
 import Shows from "../shows/Shows";
-import makeAnimated from "react-select/animated";
+import SkeltonShows from "../skelton shows/SkeltonShows";
+import { useDispatch } from "react-redux";
+import { hideScrollerApp, showScrollerApp } from "../../redux/actions/appAction";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState(null);
   const [showModal, setShowModal] = useState(false); // Add state to control modal visibility
   const [selectedOption, setSelectedOption] = useState(null); // Add state for selected option
-
+  const dispatch=useDispatch()
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -23,12 +23,14 @@ const SearchBar = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if(!searchTerm || !selectedOption)
-        return
+    if (!searchTerm || !selectedOption) return;
     try {
-      const data=await getSearchedShows(selectedOption,searchTerm)
-      setResults(data.results)
-        setShowModal(true); // Show modal on form submit
+      //hide body scroller
+      dispatch(hideScrollerApp())
+
+      setShowModal(true);
+      const data = await getSearchedShows(selectedOption, searchTerm);
+      setResults(data.results);
     } catch (error) {
       console.error(error);
     }
@@ -36,50 +38,56 @@ const SearchBar = () => {
 
   const handleCloseModal = () => {
     setShowModal(false); // Close modal
+    setResults(null)
+    //show body scroller
+    dispatch(showScrollerApp())
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="search-bar">
-        <div className="search-input-container">
-          <select
-            className="input"
-            onChange={handleSelectChange}
-          >
-            <option value="">Select Media Type</option>
-            <option value="movie">movies</option>
-            <option value="tv">tv</option>
-          </select>
+    <div className="container">
+      <div>
+        <form onSubmit={handleSubmit} className="search-bar">
+          <div className="search-input-container">
+            <select className="input" onChange={handleSelectChange}>
+              <option value="">Select Media Type</option>
+              <option value="movie">movies</option>
+              <option value="tv">tv</option>
+            </select>
 
-          <input
-            type="text"
-            placeholder="Search..."
-            value={searchTerm}
-            onChange={handleChange}
-            className="search-input"
-            style={{ borderColor: "#ffbd4d", color: "#ffbd4d" }}
-          />
-        </div>
-        <button
-          type="submit"
-          className="search-button"
-          style={{ color: "#ffbd4d" }}
-        >
-          <FiSearch />
-        </button>
-      </form>
-      {/* Render modal overlay with search results */}
-      {showModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <button onClick={handleCloseModal} className="close-button">
-              X
-            </button>
-            <div className="results">
-              {results&&<Shows shows={results} title={selectedOption.label}/>}
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={handleChange}
+              className="search-input"
+              style={{ borderColor: "#ffbd4d", color: "#ffbd4d" }}
+            />
+          </div>
+          <button
+            type="submit"
+            className="search-button"
+            style={{ color: "#ffbd4d" }}
+          >
+            <FiSearch />
+          </button>
+        </form>
+        {/* Render modal overlay with search results */}
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <button onClick={handleCloseModal} className="close-button">
+                X
+              </button>
+              <div className="results">
+                {results ? (
+                  <Shows shows={results} title={selectedOption.label} />
+                ) : (
+                  <SkeltonShows num={20} />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
