241e1402bf4084d44a6eafb0c84708099afffc26
momotan <momotaha039@gmail.com>
2023-04-17 20:11:50 +0300

    create tv component

---
+++ src/pages/tv/Tv.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RecommendedShows from "../../components/recommended shows/RecommendedShows";
+import SearchBar from "../../components/search bar/SearchBar";
+
+function Tv() {
+  return (
+    <div className="tv">
+      <SearchBar />
+      <RecommendedShows mediaType='tv'/>
+    </div>
+  );
+}
+
+export default Tv;
