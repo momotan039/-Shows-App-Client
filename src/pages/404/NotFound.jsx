948d0def453a69bd30521dd709cf1fff5f61fb3d
MOHAMMED <momotaha039@gmail.com>
2023-04-18 00:48:44 +0300

    create not found component

---
+++ src/pages/404/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './NotFound.css'
+function NotFound() {
+  return (
+    <div className='notfound container'>
+    <img src='/images/404.gif'/>
+    </div>
+  )
+}
+
+export default NotFound
