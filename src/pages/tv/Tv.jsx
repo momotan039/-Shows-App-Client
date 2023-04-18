a7aeb22f88591bc32fc7ffddcacf7958f83eed18
momotan <momotaha039@gmail.com>
2023-04-18 20:11:00 +0300

    add slide-left animation

--- src/pages/tv/Tv.jsx
+++ src/pages/tv/Tv.jsx
@@ -4,9 +4,11 @@ import SearchBar from "../../components/search bar/SearchBar";
 
 function Tv() {
   return (
-    <div className="tv">
+    <div className="slide-left">
+      <div className="tv">
       <SearchBar mediaType='tv'/>
       <RecommendedShows mediaType='tv'/>
+    </div>
     </div>
   );
 }
