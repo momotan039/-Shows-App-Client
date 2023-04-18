71887b071fa6fb7ac229594a4e9f41f5b96542e6
momotan <momotaha039@gmail.com>
2023-04-18 20:09:11 +0300

    remvoe getAllGeneres

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,6 +1,5 @@
 import { getAllGeneres } from "../api/shows"
 import { setCurrentUser } from "../redux/actions/accountActions"
-import { setGenresShows } from "../redux/actions/showActions"
 
 export const saveUserToStorage=(user)=>{
     localStorage.setItem('user',JSON.stringify(user))
@@ -9,10 +8,5 @@ export const saveUserToStorage=(user)=>{
 export const getUserfromStorage=(dispatch)=>{
     const user= JSON.parse( localStorage.getItem('user'))
     if(user)
-    {
         dispatch(setCurrentUser(user))
-        // getAllGeneres().then(gns=>{
-        //     dispatch(setGenresShows(gns))
-        // })
-    }
 }
