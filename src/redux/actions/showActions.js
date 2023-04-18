20e3d9b7dbf10744182f37eb8fecce5766009584
momotan <momotaha039@gmail.com>
2023-04-18 20:10:04 +0300

    remove unneccery actions

--- src/redux/actions/showActions.js
+++ src/redux/actions/showActions.js
@@ -1,36 +1,6 @@
 export const selectShow=(show)=>{
-     
    return{
         type:'SELECT_SHOW',
         payload:show
-   } 
-}
-
-export const addToFavorite=(show)=>{
-   return{
-        type:'ADD_TO_FAVORITE',
-        payload:show
-   } 
-}
-
-export const addToWatchLater=(show)=>{
-   return{
-        type:'ADD_TO_WATCH_LATER',
-        payload:show
-   } 
-}
-
-export const addToViewed=(show)=>{
-   return{
-        type:'ADD_TO_VIEWED',
-        payload:show
-   } 
-}
-
-
-export const setGenresShows=(genres)=>{
-   return{
-        type:'SET_GENRES_SHOWS',
-        payload:genres
    } 
 }
