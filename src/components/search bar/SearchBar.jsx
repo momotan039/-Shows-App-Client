53252eb644f99522552a94906f179a294b334996
momotan <momotaha039@gmail.com>
2023-04-18 20:18:27 +0300

    add page to when search for term

--- src/components/search bar/SearchBar.jsx
+++ src/components/search bar/SearchBar.jsx
@@ -5,14 +5,19 @@ import { getSearchedShows } from "../../api/shows";
 import Shows from "../shows/Shows";
 import SkeltonShows from "../skelton shows/SkeltonShows";
 import { useDispatch } from "react-redux";
-import { hideScrollerApp, showScrollerApp } from "../../redux/actions/appAction";
+import { hideScrollerApp, scrollToTop, showScrollerApp } from "../../redux/actions/appAction";
+import Pagination from "../pagination/Pagination";
+import usePagination from "../../hooks/usePagination";
 
 const SearchBar = ({mediaType}) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState(null);
   const [showModal, setShowModal] = useState(false); // Add state to control modal visibility
   const [selectedOption, setSelectedOption] = useState(mediaType); // Add state for selected option
+  const [pages,setPages,loading,setLoading]=usePagination()
+
   const dispatch=useDispatch()
+
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -20,17 +25,28 @@ const SearchBar = ({mediaType}) => {
   const handleSelectChange = (e) => {
     setSelectedOption(e.target.value);
   };
-
-  const handleSubmit = async (event) => {
+const clearClassAnimation=()=>{
+      let elem=document.querySelector('.slide-right')
+      if(elem)
+      elem.classList=''
+      elem=document.querySelector('.slide-left')
+      if(elem)
+      elem.classList=''
+}
+  const handleSubmit = async (page) => {
     event.preventDefault();
     if (!searchTerm || !selectedOption) return;
     try {
+      setResults(null)
+      dispatch(scrollToTop('.modal-content'))
       //hide body scroller
       dispatch(hideScrollerApp())
-
       setShowModal(true);
-      const data = await getSearchedShows(selectedOption, searchTerm);
+      //remove animation class
+      clearClassAnimation()
+      const data = await getSearchedShows(selectedOption, searchTerm,page);
       setResults(data.results);
+      setPages({ current: data.page, total: data.total_pages });
     } catch (error) {
       console.error(error);
     }
@@ -44,8 +60,7 @@ const SearchBar = ({mediaType}) => {
   };
   return (
     <div className="container">
-      <div>
-        <form onSubmit={handleSubmit} className="search-bar">
+        <form onSubmit={()=>handleSubmit(1)} className="search-bar">
           <div className="search-input-container">
             <select  className="input" value={mediaType} onChange={handleSelectChange}>
               <option value="">Select Media Type</option>
@@ -79,15 +94,26 @@ const SearchBar = ({mediaType}) => {
               </button>
               <div className="results">
                 {results ? (
-                  <Shows shows={results} title={`searching for : ${searchTerm}`}/>
+                  <Shows showNotFound={true} shows={results} title={`searching for : ${searchTerm}`}/>
                 ) : (
                   <SkeltonShows num={20} />
                 )}
               </div>
             </div>
+            {pages && (
+        <div className="container pagination">
+          <Pagination
+          _currentPage={pages.current}
+          isLoading={loading}
+          totalPages={pages.total}
+          onPageChange={(page) => {
+            handleSubmit(page)
+          }}
+        />
+        </div>
+      )}
           </div>
         )}
-      </div>
     </div>
   );
 };
