698b805eecc813eb92a3aef2a9801a8c00bafa41
MOHAMMED <momotaha039@gmail.com>
2023-04-19 01:21:44 +0300

    add mediatype to trending shows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,15 @@ import { fetchUsers } from "../api/auth";
 import SearchBar from "../components/search bar/SearchBar";
 import TrendingShows from "../components/trending shows/TrendingShows";
 import RecommendedShows from "../components/recommended shows/RecommendedShows";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   return (
     <div className="slide-right">
       <div className="home">
         <SearchBar mediaType="movie" />
-        <TrendingShows />
+        <TrendingShows mediaType='movie' />
         <RecommendedShows mediaType="movie" />
       </div>
     </div>
