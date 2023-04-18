be7e257a21a301f0c74fe671e131b3de51070abe
MOHAMMED <momotaha039@gmail.com>
2023-04-19 01:21:51 +0300

    add mediatype to trending shows

--- src/pages/tv/Tv.jsx
+++ src/pages/tv/Tv.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import RecommendedShows from "../../components/recommended shows/RecommendedShows";
 import SearchBar from "../../components/search bar/SearchBar";
+import TrendingShows from "../../components/trending shows/TrendingShows";
 
 function Tv() {
   return (
     <div className="slide-left">
       <div className="tv">
       <SearchBar mediaType='tv'/>
+      <TrendingShows mediaType='tv'/>
       <RecommendedShows mediaType='tv'/>
     </div>
     </div>
