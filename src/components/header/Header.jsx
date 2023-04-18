37f65c1098745ace0283cdbc927fe569916b6430
momotan <momotaha039@gmail.com>
2023-04-18 20:14:50 +0300

    add menu-bar div and openSmallMenu state

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./Header.css"; // Import CSS file for styling
 import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import { AiOutlineLogout } from "react-icons/ai";
+import { AiOutlineLogout,AiOutlineMenuFold,AiOutlineClose } from "react-icons/ai";
 import { logout } from "../../redux/actions/authActions";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigator = useNavigate()
   const { user } = useSelector((state) => state.account);
+  const [openSmallMenu,setOpenSmallMenu]=useState(false)
   return (
     <div className="container">
       <header className={`${user ? "space" : ""}`}>
@@ -19,7 +20,7 @@ const Header = () => {
       </div>
       {user && (
         <>
-          <div className="controls">
+          <div className={`controls ${openSmallMenu?'small-menu':''}`}>
             <nav className="menu">
               <ul>
                 <NavLink to="/">Home</NavLink>
@@ -29,9 +30,16 @@ const Header = () => {
               </ul>
             </nav>
             <div className="log-out">
-              <h3>hello {user.name.split(" ")[0]}</h3>
+              {/* <h3>hello {user.name.split(" ")[0]}</h3> */}
               <AiOutlineLogout onClick={async () => await logout(dispatch,navigator)} size={30} />
             </div>
+          </div>
+          <div className="menu-bar">
+          <AiOutlineMenuFold onClick={()=>setOpenSmallMenu(true)} size={30}/>
+          {
+            openSmallMenu&&
+            <AiOutlineClose className="close-menu" onClick={()=>setOpenSmallMenu(false)} size={30}/>
+          }
           </div>
         </>
       )}
