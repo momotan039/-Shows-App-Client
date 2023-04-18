6e4a781d6bda2cf98dd0a8a309e2b31a7a8db0f9
momotan <momotaha039@gmail.com>
2023-04-18 20:20:04 +0300

    refactor it

--- src/components/user info card/UserInfoCard.jsx
+++ src/components/user info card/UserInfoCard.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import "./UserInfoCard.css"; 
+import React from "react";
+import { useSelector } from "react-redux";
+import "./UserInfoCard.css";
 
-function UserInfoCard({classs}) {
-    const { user } = useSelector((state) => state.account);
-    const { email, name } = user;
-    const { lang, show_lang, genres } = user.preferences;
+const UserInfoCard = ({ classs }) => {
+  const { user } = useSelector((state) => state.account);
+  const { email, name } = user;
+  const { lang, show_lang, genres } = user.preferences ?? {};
+  
   return (
     <div className={classs}>
-    <div className={`user-info-container`}>
-    <h2 className="user-info-title">Welcome to Your Dashboard</h2>
-    <div className="user-info-item">
-      <span className="user-info-label">Email:</span> {email}
+      <h2 className="title">Welcome to Your Dashboard</h2>
+      <div className="user-info-container">
+        <div className="user-info-avatar">
+          <img src='./images/avatar.gif' alt="Avatar" className="avatar-image" />
+          <div className="avatar-username">{name}</div>
+        </div>
+        <div className="user-info-details">
+          <div className="user-info-item">
+            <span className="user-info-label">Email:</span> {email}
+          </div>
+          <div className="user-info-item">
+            <span className="user-info-label">Language:</span> {lang}
+          </div>
+          <div className="user-info-item">
+            <span className="user-info-label">Show Language:</span> {show_lang}
+          </div>
+          <div className="user-info-item">
+            <span className="user-info-label">Genres:</span>
+            <ul>
+              {genres?.map((genre, index) => (
+                <li key={index}>{genre.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="user-info-footer">Enjoy Your Experience!</div>
+        </div>
+      </div>
     </div>
-    <div className="user-info-item">
-      <span className="user-info-label">Name:</span> {name}
-    </div>
-    <div className="user-info-item">
-      <span className="user-info-label">Language:</span> {lang}
-    </div>
-    <div className="user-info-item">
-      <span className="user-info-label">Show Language:</span> {show_lang}
-    </div>
-    <div className="user-info-item">
-      <span className="user-info-label">Genres:</span>
-      <ul>
-        {genres.map((genre, index) => (
-          <li key={index}>
-            ID: {genre.id}, Name: {genre.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-    <div className="user-info-footer">Enjoy Your Experience!</div>
-  </div>
-    </div>
-  )
+  );
 }
 
-export default UserInfoCard
+export default UserInfoCard;
