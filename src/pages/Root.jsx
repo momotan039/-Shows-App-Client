3233b54e4e8ff426069d29f8cd61c7c9de941b09
MOHAMMED <momotaha039@gmail.com>
2023-04-19 01:22:48 +0300

    check if user adjust the preferences

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,18 +1,28 @@
 import React, { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import Loader from "../components/loader/Loader";
+import Popup from "../components/popup/PopUp";
+import { useSelector } from "react-redux";
 
 function Root() {
+  const {user}=useSelector(s=>s.account)
+  const nav=useNavigate()
+  useEffect(()=>{
+    if(user&&!user.preferences)
+    nav('/setup-profile')
+  },[user])
+
   return (
-    <div className="root">
+  <div className="root">
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
       <Loader />
+      <Popup/>
     </div>
   );
 }
