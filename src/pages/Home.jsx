ea61ed3739e88c7c84a95c7d17224b85fd425bd4
momotan <momotaha039@gmail.com>
2023-04-18 20:21:35 +0300

    add slide-right animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react'
-import { fetchUsers } from '../api/auth'
-import SearchBar from '../components/search bar/SearchBar'
-import TrendingShows from '../components/trending shows/TrendingShows'
-import RecommendedShows from '../components/recommended shows/RecommendedShows'
+import React, { useEffect } from "react";
+import { fetchUsers } from "../api/auth";
+import SearchBar from "../components/search bar/SearchBar";
+import TrendingShows from "../components/trending shows/TrendingShows";
+import RecommendedShows from "../components/recommended shows/RecommendedShows";
 
 function Home() {
   return (
-    <div className="home">
-      <SearchBar mediaType='movie'/>
-      <TrendingShows/>
-      <RecommendedShows mediaType='movie'/>
+    <div className="slide-right">
+      <div className="home">
+        <SearchBar mediaType="movie" />
+        <TrendingShows />
+        <RecommendedShows mediaType="movie" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
