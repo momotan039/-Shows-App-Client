f178091fcc54aa905c23ed86555beb9253a1d2f5
MOHAMMED <momotaha039@gmail.com>
2023-04-19 01:20:43 +0300

    remove delaying time

--- src/components/trending shows/TrendingShows.jsx
+++ src/components/trending shows/TrendingShows.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Shows from "../shows/Shows";
 import { getTrendingShows } from "../../api/shows";
-import { useDispatch } from "react-redux";
-import { showLoader } from "../../redux/actions/loaderActions";
 
-function TrendingShows() {
+function TrendingShows({mediaType}) {
   const [shows, setShows] = useState(null);
-  const dispatch = useDispatch();
   const _getTrendingShows = async () => {
     try {
-      const data = await getTrendingShows();
-      setTimeout(() => {
+      const data = await getTrendingShows(mediaType);
         setShows(data.results);
-      }, 2000);
     } catch (error) {
       alert(error.response.data.message);
     }
