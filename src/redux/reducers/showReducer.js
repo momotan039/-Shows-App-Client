6d7b59fa4268f52f237559f8ffb43cc9ea63a59f
momotan <momotaha039@gmail.com>
2023-04-18 20:09:49 +0300

    remove unneccery actions

--- src/redux/reducers/showReducer.js
+++ src/redux/reducers/showReducer.js
@@ -7,14 +7,6 @@ export default (state = initialState, { type, payload }) => {
   switch (type) {
   case 'SELECT_SHOW':
     return { ...state, show:payload }
-  case 'SET_GENRES_SHOWS':
-    return {...state,genres:payload}
-  case 'ADD_TO_FAVORITE':
-    return {...state,show:payload}
-  case 'ADD_TO_WATCH_LATER':
-    return {...state,show:payload}
-  case 'ADD_TO_VIEWED':
-    return {...state,show:payload}
   default:
     return state
   }
