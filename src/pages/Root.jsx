1d257f36ffa1c187088b298f3e905633fb79c0e8
momotan <momotaha039@gmail.com>
2023-04-22 14:10:06 +0300

    use popup

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -3,6 +3,7 @@ import { Outlet, useNavigate } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import Loader from "../components/loader/Loader";
+import Popup from "../components/popup/Popup";
 import { useSelector } from "react-redux";
 
 function Root() {
@@ -21,6 +22,7 @@ function Root() {
       </main>
       <Footer />
       <Loader />
+      <Popup/>
     </div>
   );
 }
