431ef10b10f077145e9a61fdf6b62e7e48076a56
momotan <momotaha039@gmail.com>
2023-04-22 11:16:06 +0300

    remove pop up

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -3,7 +3,6 @@ import { Outlet, useNavigate } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import Loader from "../components/loader/Loader";
-import Popup from "../components/popup/PopUp";
 import { useSelector } from "react-redux";
 
 function Root() {
@@ -22,7 +21,6 @@ function Root() {
       </main>
       <Footer />
       <Loader />
-      <Popup/>
     </div>
   );
 }
