0a95e7f9e4b0c2d7c86ba582a6b70e7c2c7f1f9a
momotan <momotaha039@gmail.com>
2023-04-22 14:09:14 +0300

    edit popup

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,24 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RiCloseFill } from 'react-icons/ri'; // Import the close icon from React Icons library
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { HidePopUp } from '../../redux/actions/appAction';
+import store from '../../redux/store';
 import './Popup.css'; // Import CSS file for styling
 
 const Popup = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
+  const {popup}=useSelector(s=>s.app)
+  const [isOpen, setIsOpen] = useState(false);
+  const dispatch=useDispatch()
+  useEffect(() => {
+    setIsOpen(popup.isShow)
+  })
+  
   const handleClose = () => {
-    setIsOpen(false);
+    store.dispatch(HidePopUp())
   };
+  
 
   return (
     <>
       {isOpen && (
         <div className="popup-overlay">
-          <div className="popup-container">
-            <button className="close-button" onClick={handleClose}>
-              <RiCloseFill className="close-icon" /> {/* Render the close icon */}
+          <div className="popup-container scale-up">
+            <button className="popup-close-button" onClick={handleClose}>
+              <RiCloseFill className="close-icon" /> 
             </button>
             <div className="popup-content">
-              {/* {children} */}
+              <h1 className='shows-title'>{popup.message}</h1>
+              <Link onClick={()=>dispatch(HidePopUp())} className='button-ok' to='/'>GO To Home</Link>
             </div>
           </div>
         </div>
