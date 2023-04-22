7d526ab7790173c6b4c479a06947e2490c80a0ce
momotan <momotaha039@gmail.com>
2023-04-22 11:49:40 +0300

    remove language

--- src/components/user info card/UserInfoCard.jsx
+++ src/components/user info card/UserInfoCard.jsx
@@ -13,15 +13,12 @@ const UserInfoCard = ({ classs }) => {
       <h2 className="title">Welcome to Your Dashboard</h2>
       <div className="user-info-container">
         <div className="user-info-avatar">
-          <img src='./images/avatar.gif' alt="Avatar" className="avatar-image" />
+          <img src='/images/avatar.gif' alt="Avatar" className="avatar-image" />
           <div className="avatar-username">{name}</div>
         </div>
         <div className="user-info-details">
           <div className="user-info-item">
             <span className="user-info-label">Email:</span> {email}
-          </div>
-          <div className="user-info-item">
-            <span className="user-info-label">Language:</span> {lang}
           </div>
           <div className="user-info-item">
             <span className="user-info-label">Show Language:</span> {show_lang}
